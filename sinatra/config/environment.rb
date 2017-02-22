52b95b5876862b616721a59271b8318f458a69e5
github username <github email address>
2017-02-22 13:08:54 -0500

    First Commit

--- sinatra/config/environment.rb
+++ sinatra/config/environment.rb
@@ -5,7 +5,7 @@ require "sinatra/activerecord"
 # 1. load up all of the relevant libraries
 # 2. Require my application
 
-# set :database, {adapter: "sqlite3", database: "spiders.sqlite3"}
+set :database, {adapter: "sqlite3", database: "spiders.sqlite3"}
 
 
 require_all('app/')
