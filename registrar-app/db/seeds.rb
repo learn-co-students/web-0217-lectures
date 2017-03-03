f523077910f4c8b438fe1b89d1d283fa7678222a
Jeff Katz <jeffreyerickatz@gmail.com>
2017-03-03 11:01:16 -0500

    adding registrar app

---
+++ registrar-app/db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Department.create(name: 'Computer Science')
+Department.create(name: 'Foreign Languages')
+Department.create(name: 'Math')
+Department.create(name: 'Science')
+Department.create(name: 'History')
